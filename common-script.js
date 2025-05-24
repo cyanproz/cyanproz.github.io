37a086cb0080f62f0fb0011d8ef57bcb3d9d8a6d
cyanproz <alif.pradani@outlook.com>
2025-05-24 17:07:00 +0700

    Update common-script.js

--- common-script.js
+++ common-script.js
@@ -55,46 +55,46 @@ function view_image(URL) {
 
 function getWindowsOS() {
     // http://msdn.microsoft.com/en-us/library/ms537503(v=vs.85).aspx#PltToken
-    if (navigator.appVersion.indexOf("Windows NT 10.")!=-1) {
+    if (navigator.appVersion.indexOf("Windows NT 10.") != -1) {
         return 'Windows 10, Windows 11';
     }
-    else if (navigator.appVersion.indexOf("Windows NT 6.3")!=-1) {
+    else if (navigator.appVersion.indexOf("Windows NT 6.3") != -1) {
         return "Windows 8.1";
     }
-    else if (navigator.appVersion.indexOf("Windows NT 6.2")!=-1) {
+    else if (navigator.appVersion.indexOf("Windows NT 6.2") != -1) {
         return "Windows 8";
     }
-    else if (navigator.appVersion.indexOf("Windows NT 6.1")!=-1) {
+    else if (navigator.appVersion.indexOf("Windows NT 6.1") != -1) {
         return "Windows 7";
     }
-    else if (navigator.appVersion.indexOf("Windows NT 6.0")!=-1) {
+    else if (navigator.appVersion.indexOf("Windows NT 6.0") != -1) {
         return "Windows Vista";
     }
-    else if (navigator.appVersion.indexOf("Windows NT 5.2")!=-1) {
+    else if (navigator.appVersion.indexOf("Windows NT 5.2") != -1) {
     	return "Windows Server 2003; Windows XP x64 Edition";
     }
-    else if (navigator.appVersion.indexOf("Windows NT 5.1")!=-1) {
+    else if (navigator.appVersion.indexOf("Windows NT 5.1") != -1) {
         return "Windows XP";
     }
-    else if (navigator.appVersion.indexOf("Windows NT 5.01")!=-1) {
+    else if (navigator.appVersion.indexOf("Windows NT 5.01") != -1) {
         return "Windows 2000, Service Pack 1 (SP1)";
     }
-    else if (navigator.appVersion.indexOf("Windows NT 5.0")!=-1) {
+    else if (navigator.appVersion.indexOf("Windows NT 5.0") != -1) {
     	return "Windows 2000";
     }
-    else if (navigator.appVersion.indexOf("Windows NT 4.0")!=-1) {
+    else if (navigator.appVersion.indexOf("Windows NT 4.0") != -1) {
         return "Windows NT 4.0";
     }
-    else if (navigator.appVersion.indexOf("Windows 98")!=-1) {
+    else if (navigator.appVersion.indexOf("Windows 98") != -1) {
         return "Windows 98";
     }
-    else if (navigator.appVersion.indexOf("Windows 98; Win 9x 4.90")!=-1) {
+    else if (navigator.appVersion.indexOf("Windows 98; Win 9x 4.90") != -1) {
         return "Windows Millennium Edition (Windows Me)";
     }
-    else if (navigator.appVersion.indexOf("Windows 95")!=-1) {
+    else if (navigator.appVersion.indexOf("Windows 95") != -1) {
         return "Windows 95";
     }
-    else if (navigator.appVersion.indexOf("Windows CE")!=-1) {
+    else if (navigator.appVersion.indexOf("Windows CE") != -1) {
         return "Windows CE";
     }
     else {
