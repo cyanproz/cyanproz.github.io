b97ea71eb3ad323af043673f6d860f43af427649
cyanproz <alif.pradani@outlook.com>
2025-06-14 06:09:04 +0700

    Update common-script-lib.js

--- common-script-lib.js
+++ common-script-lib.js
@@ -1,11 +1,19 @@
-class GlobalLib {
-    ajaxJsonPhpErrorHandler(response) {
+export class GlobalLib {
+    static delay(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
+    static async awaitWhile(conditionFunc, interval = 100) {
+        while (conditionFunc()) {
+            await delay(interval);
+        }
+    }
+
+    static ajaxJsonPhpErrorHandler(response) {
         return response.clone().json().catch(() => {
             return response.text().then(text => {
-                throw new Error("Server error (not JSON):\n" + text);
+                throw new Error(text);
             });
         });
     }
 }
-
-export { GlobalLib };
